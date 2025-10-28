bd1087450847dbd1a48e37af7043585131e5364e
begidudimtse <begidudimtse@gmail.com>
2025-10-28 10:49:15 +0300

    edit

--- alx-rick-and-morty-app/pages/_app.tsx
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-import ErrorBoundary from '@/components/ErrorBoundary';
+import ErrorBoundary from '../components/ErrorBoundary';
 import type { AppProps } from "next/app";
 
 
